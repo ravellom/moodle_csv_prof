8bb7c702ce0370eda724df3654cb8a6bc89ed834
rcmarc <ramoscarrer@gmail.com>
2021-11-12 08:15:10 -0500

    added an entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,22 @@
 #!/bin/sh
-python manage.py runserver 0.0.0.0:8000
+
+# Migrate database
+echo "Creating database..."
+python manage.py migrate
+
+# Ceate superuser
+echo "Creating super user..."
+python manage.py createsuperuser --noinput
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+# Start the server
+echo "Starting the server..."
+if [ $DEPLOY = "0" ]
+then
+    python manage.py runserver 0.0.0.0:8000
+else
+    uwsgi --socket :29000 --module moodle_csv_prof.wsgi --master --enable-threads --uid=505 --gid=505
+fi
