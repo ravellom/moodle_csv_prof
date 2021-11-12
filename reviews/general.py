d8a64dee10386bffe2b833990413555edb62b94b
Raidell Avello <ravellom@gmail.com>
2021-11-12 16:25:25 -0500

    Actualización general
    Aspecto de los gráficos y layout

--- reviews/general.py
+++ reviews/general.py
@@ -14,32 +14,38 @@ def plot_general_1(df):
                         "yaxis": {  "title":"Accesos"}
                         })
     access_mean = course_total_access.N.mean()
+    fig.data[0].update(mode='markers+lines')
     fig.add_hline(y = access_mean, line_color = "red", line_width = 1, line_dash='dash')
-    fig.update_layout(height=300)
+    fig.update_layout(height=400)
     fig.update_layout(my_globals.BASE_LAYOUT)
-    plot_div = plot(fig, output_type="div")#, config={"displayModeBar": False})
+    plot_div = plot(fig, output_type="div", config={'responsive':True})#, config={"displayModeBar": False})
     return plot_div
 
 def plot_general_heatmap(df):
     course_headmap = data.get_course_headmap(df)
     trace1 = go.Heatmap(
         z = course_headmap['N'],
-        x = course_headmap['hour'],
-        y = course_headmap['wd'], # zmin=-1, # zmax=1,
+        x = course_headmap['wd'],
+        y = course_headmap['hour'], # zmin=-1, # zmax=1,
         xgap = 1, # Sets the horizontal gap (in pixels) between bricks
         ygap = 1,
-        colorscale = 'blues' #'RdBu'
+        colorscale = 'blues', #'RdBu'
+        colorbar=dict(thickness=5)
     )
     fig = go.Figure(data=trace1)
-    fig.update_layout(height=300)
     fig.update_layout(my_globals.BASE_LAYOUT)
-    fig.update_layout(  yaxis=dict( ticktext = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'],
+    fig.update_layout(  xaxis=dict( ticktext = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'],
                                     tickmode = 'array',
                                     tickvals = [0,1,2,3,4,5,6] ),
-                        xaxis=dict( 
+                        yaxis=dict( 
+                                    ticktext = ['12am','2am','4am','6am','8am','10am',
+                                                '12m','2pm','4pm','6pm', '8pm','10pm'],
+                                    #ticktext = ['12am','1am','2am','3am','4am','5am','6am','7am','8am','9am','10am','11am',
+                                     #           '12m','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','9pm','10pm','11pm'],
                                     tickmode = 'array',
-                                    tickvals = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23] ))
-                        
+                                    #tickvals = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23] ))
+                                    tickvals = [0,2,4,6,8,10,12,14,16,18,20,22] ))
+    fig.update_layout(height=400,width=300)
     plot_div = plot(fig, output_type="div", config={'responsive':True})#, config={"displayModeBar": False})
     return plot_div
 
@@ -50,7 +56,7 @@ def plot_country_count_IP(df):
                     color="N", # lifeExp is a column of gapminder
                     hover_name="Country", # column to add to hover information
                     color_continuous_scale=px.colors.sequential.Plasma)
-    fig.update_layout(height=300)
+    #fig.update_layout(height=400)
     fig.update_layout(my_globals.BASE_LAYOUT)
     plot_div = plot(fig, output_type="div")
     return plot_div
